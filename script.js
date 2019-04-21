7fd7513c9bb9a624495d5b57d1cc96d032fa6a53
arthur <arthur@techie.com>
2019-04-21 03:12:25 -0400

    Create Operator Function In script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//Will be instantiated for addition, subtraction, multiplication, division
+function Operate(num1, operand, num2){
+        this.num1 =  num1;
+        this.operand =   operand;
+        this.num2 = num2;
+}
+
